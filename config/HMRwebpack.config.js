3fd45f873ce674fadcad83ce6eb3a5860432e47c
杨冠群 <2055989300@qq.com>
2021-01-02 14:39:46 +0800

    oneof提升构建速度

---
+++ config/HMRwebpack.config.js
@@ -0,0 +1,135 @@
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+process.env.NODE_ENV = 'development';
+
+// 复用loader
+const commonCssLoader = [
+  {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      publicPath: '../'
+    }
+  },
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        ident: 'postcss',
+        plugins: () => {
+          require('postcss-preset-env')();
+        }
+      }
+    }
+  }
+]
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/js/index.js', './src/main.html'],
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build')
+  },
+  module: {
+    /* 
+      正常来讲，一个文件只能被一个loader处理。
+      当一个文件要被多个loader处理，那么一定要指定loader执行的先后顺序：
+        先执行eslint 再执行babel（因为，我们只要检查我们自己的源码）
+    */
+    rules: [
+      {
+        test: /\.css$/,
+        use: [...commonCssLoader]
+      },
+      {
+        test: /\.less$/,
+        use: [...commonCssLoader, 'less-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // 优先执行
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                // 按需加载 
+                useBuiltIns: 'usage', // 指定 core-js 版本 
+                corejs: {
+                  version: 3
+                },
+                // 指定兼容性做到哪个版本浏览器 
+                targets: {
+                  chrome: '60',
+                  firefox: '60',
+                  ie: '9',
+                  safari: '10',
+                  edge: '17'
+                }
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs',
+          esModule: false
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        exclude: /\.(css|js|html|less|json|jpg|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/main.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    })
+  ],
+  devServer: {
+    contentBase: resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+    open: true,
+    // 开启HMR功能
+    hot: true
+  },
+  devtool: 'inline-source-map'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -35,24 +35,10 @@ module.exports = {
     path: resolve(__dirname, 'build')
   },
   module: {
-    /* 
-      正常来讲，一个文件只能被一个loader处理。
-      当一个文件要被多个loader处理，那么一定要指定loader执行的先后顺序：
-        先执行eslint 再执行babel（因为，我们只要检查我们自己的源码）
-    */
     rules: [
-      {
-        test: /\.css$/,
-        use: [...commonCssLoader]
-      },
-      {
-        test: /\.less$/,
-        use: [...commonCssLoader, 'less-loader']
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        // 优先执行
         enforce: 'pre',
         loader: 'eslint-loader',
         options: {
@@ -60,53 +46,65 @@ module.exports = {
         }
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                // 按需加载 
-                useBuiltIns: 'usage', // 指定 core-js 版本 
-                corejs: {
-                  version: 3
-                },
-                // 指定兼容性做到哪个版本浏览器 
-                targets: {
-                  chrome: '60',
-                  firefox: '60',
-                  ie: '9',
-                  safari: '10',
-                  edge: '17'
-                }
-              }
-            ]
-          ]
-        }
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8 * 1024,
-          name: '[hash:10].[ext]',
-          outputPath: 'imgs',
-          esModule: false
-        }
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        exclude: /\.(css|js|html|less|json|jpg|png|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash:10].[ext]',
-          outputPath: 'media'
-        }
+        // 以下loader只会匹配一个 提升构建速度
+        // 注意：不能有两个配置处理同一种类型的文件（如eslint和Babel都会对js 进行处理 所以要把eslint 放到外面去
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [...commonCssLoader]
+          },
+          {
+            test: /\.less$/,
+            use: [...commonCssLoader, 'less-loader']
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: {
+                      version: 3
+                    },
+                    targets: {
+                      chrome: '60',
+                      firefox: '60',
+                      ie: '9',
+                      safari: '10',
+                      edge: '17'
+                    }
+                  }
+                ]
+              ]
+            }
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[hash:10].[ext]',
+              outputPath: 'imgs',
+              esModule: false
+            }
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          },
+          {
+            exclude: /\.(css|js|html|less|json|jpg|png|gif)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[hash:10].[ext]',
+              outputPath: 'media'
+            }
+          }
+        ]
       }
     ]
   },
@@ -128,7 +126,6 @@ module.exports = {
     compress: true,
     port: 3000,
     open: true,
-    // 开启HMR功能
     hot: true
   },
   devtool: 'inline-source-map'
